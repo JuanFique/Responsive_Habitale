029d1b8a8689738ffdebf0105248e310dc175484
JuanFique <est.juan.fique@unimilitar.edu.co>
2025-04-05 23:42:02 -0500

    Puliendo paginas terminadas

--- js/menu.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
-    const menu = document.getElementById('dropdown-menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-});
